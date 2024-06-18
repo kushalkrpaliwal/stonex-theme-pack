e47dfedd3001d778aa3503eca21e27b4695cbbc2
Michal Majer <Michal.Majer@StoneX.com>
2024-06-18 17:06:14 +0200

    Update tokens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import type { AppProps } from 'next/app';
 import { MeshApp } from '@uniformdev/mesh-sdk-react';
 import '../styles/globals.scss';
-// import '../fe-app/src/styles/globals.scss';
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <MeshApp>
